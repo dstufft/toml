8ef054379970619749af71b3ed7d2b1888f6001c
Donald Stufft <donald@stufft.io>
2017-03-25 19:34:36 -0400

    Setup the project skeleton

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+
+setup(
+    name="toml",
+    version="0.1.dev0",
+
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
+
+    install_requires=["rply"],
+)
